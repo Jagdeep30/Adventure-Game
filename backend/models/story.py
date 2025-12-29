8c25024a508c5d198f35651782b49479609154d3
Jagdeep Singh <jagdeepsingh1630@gmail.com>
2025-12-29 18:18:57 +0530

    session id type change

--- backend/models/story.py
+++ backend/models/story.py
@@ -10,7 +10,7 @@ class Story(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-    session_id = Column(Integer, index=True)
+    session_id = Column(String, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     nodes = relationship("StoryNode", back_populates="story")
